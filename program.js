2271ce0aa3cca7811ab26ea82cd1fe640bcdd132
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-05-06 19:55:22 +0200

    Change program.js Add instruction

--- program.js
+++ program.js
@@ -1,13 +1,18 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    process.stdout.write('wersja Node: '+ process.version+  ' Język systemowy: '+ process.env.LANG +' ');
     var input = process.stdin.read();
     
     //console.log(process.env.LANG);
     if (input !== null) {
         var instruction = input.toString().trim();
         switch (instruction) {
+            case 'node version':
+            process.stdout.write(' Node version: '+ process.version );
+            break;
+            case 'system lang' :
+            process.stdout.write(' System language: '+ process.env.LANG +' ');
+            break;
             case '/exit':
             process.stdout.write('Quitting app!\n');
             process.exit();
